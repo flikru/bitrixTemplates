86406db9b0b22e0f1401cfa51c48358f1e31c1f9
vladimir <frostt315@gmail.com>
2024-11-07 08:46:18 +0500

    add

--- help/templates/metaPager/component_epilog.php
+++ help/templates/metaPager/component_epilog.php
@@ -3,5 +3,14 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
     die();
 
 if(isset($arResult['NAV_ACTIVE_PAGE']) && $arResult['NAV_ACTIVE_PAGE']>1){
+    /*
+     * Старое ядро
+     * */
     $APPLICATION->AddHeadString("<meta name='yandex' content='noindex, follow'/>",false);
+
+    /*
+     * D7
+     * */
+    use \Bitrix\Main\Page\Asset;
+    Asset::getInstance()->addString('<li nk rel="stylesheet" type="text/css" href="/css/style.css">');
 }
